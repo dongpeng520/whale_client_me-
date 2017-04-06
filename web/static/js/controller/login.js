6832266b0429ab71a7e73c351f08399871cd364c
董鹏 <dongpeng@juxinli.com>
2017-04-06 13:06:04 +0800

    add

---
+++ web/static/js/controller/login.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+whaleModule.controller("logincontroller",["$scope","$window","$http","$interval","$location", function($scope,$window,$http,$interval,$location){
+    $scope.login="hjkhjkhjkhjkkkkkkkkkkkkk";
+}])
---
+++ web/static/js/controller/reser.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+whaleModule.controller("resercontroller",["$scope","$window","$http","$interval","$location", function($scope,$window,$http,$interval,$location){
+    $scope.reser="我和瓯江口很难看回家看看就回家看看 ";
+}])
