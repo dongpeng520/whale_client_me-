5b989d93686a40e302dc0c63822019480f5773e2
董鹏 <dongpeng@juxinli.com>
2017-04-10 18:58:10 +0800

    add

--- web/static/js/controller/overview.js
+++ web/static/js/controller/overview.js
@@ -2,5 +2,12 @@
  * Created by Administrator on 2017/4/10.
  */
 whaleModule.controller("OverviewController",["$scope","$rootScope","$window","$http","$interval","$location", function($scope,$rootScope,$window,$http,$interval,$location){
-
+    $scope.Crawler_close=function(){
+        //$scope.crawler_close=true;
+        $rootScope.crawler_close=true;
+        $("body").css("overflow","hidden");
+    }
+    $scope.select=function(){
+        $scope.over_select=!$scope.over_select
+    }
 }])
